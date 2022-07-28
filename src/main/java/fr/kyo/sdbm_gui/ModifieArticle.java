f09e8d1db4b2919125de5263eca9c4fc562fb197
Yoann <yoann.lengrand@icloud.com>
2022-07-28 12:39:41 +0200

    Modification of the label in the modify modal so that it displays: modification of the article ..

--- src/main/java/fr/kyo/sdbm_gui/ModifieArticle.java
+++ src/main/java/fr/kyo/sdbm_gui/ModifieArticle.java
@@ -6,6 +6,7 @@ import fr.kyo.sdbm_gui.service.ServiceArticle;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -26,6 +27,8 @@ public class ModifieArticle {
     private ComboBox<Type> comboBoxType;
     @FXML
     private ComboBox<Marque> comboBoxMarque;
+    @FXML
+    private Label nomFenetre;
     private Boolean create ;
     private Article article;
     private GestionArticleController gestionArticleController;
@@ -76,6 +79,7 @@ public class ModifieArticle {
         if (article.getId() == 0){
             create = true;
         } else {
+            nomFenetre.setText("Modification de l'article : " + article.getLibelle());
             textFieldNom.setText(article.getLibelle());
             textFieldPrix.setText(String.valueOf(article.getPrixAchat()));
             comboBoxVolume.getSelectionModel().select(new Volume(article.getVolume(), String.valueOf(article.getVolume())));
